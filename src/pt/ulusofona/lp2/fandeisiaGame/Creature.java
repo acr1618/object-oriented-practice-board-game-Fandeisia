92d8f844f2e754ddb0d91d490da0fbb325491e9d
a21900126 <acr929@gmail.com>
2020-01-17 04:53:28 +0000

    CodeToHumans. Creature OK.

--- src/pt/ulusofona/lp2/fandeisiaGame/Creature.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/Creature.java
@@ -3,25 +3,23 @@ package pt.ulusofona.lp2.fandeisiaGame;
 import java.util.List;
 public abstract class Creature extends Element {
 
-
-
-     int teamId;
-     int cost;
-     String orientation;
-     String image;
-     int points;
-     String typeName;
-     int range;
-     boolean isEnchant;
-     boolean isFrozen, isFrozen4Ever;
-     int frozenTime;
-     String itSpellName;
-     int nextX, nextY;
-     int gold =0, silver =0, bronze =0;
-     String outroTypeName; // para usar com a atualização de imagens
-     int treasureCounter = 0;
-     int spellTargetCounter = 0;
-     int distanceTraveled = 0;
+    int teamId;
+    int cost;
+    String orientation;
+    String image;
+    int points;
+    String typeName;
+    int range;
+    boolean isEnchant;
+    boolean isFrozen, isFrozen4Ever;
+    int frozenTime;
+    String itSpellName;
+    int nextX, nextY;
+    int gold, silver, bronze;
+    String outroTypeName; // para usar com a atualização de imagens
+    int treasureCounter = 0;
+    int spellTargetCounter = 0;
+    int distanceTraveled = 0;
     private int collectedTreasures = 0;
 
     /*// Variáveis para guardar o mundo:
@@ -48,32 +46,43 @@ public abstract class Creature extends Element {
         this.nextY=0;
     }
 
-
-    public String getOutroTypeName() {
-        return outroTypeName;
+    public String getTypeName() {
+        return typeName;
     }
-
-    public void setOutroTypeName(String outroTypeName) {
-        this.outroTypeName = outroTypeName;
+    public int getTeamId() {
+        return teamId;
     }
-
-    public String getImagePNG(){
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
+    public int getCost(){ return cost; }
     @Override
     public String toString() {
         return id + " | " + typeName + " | " + teamId + " | " + points + " @ " + "(" + x +", " + y + ") " + orientation;
     }
 
-    public String getTypeName() {
-        return typeName;
+    /*Imagem*/
+    public String getImagePNG(){
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
     }
 
+    /* Métodos auxiliares para setar imagem*/
+    public String getOutroTypeName() {
+        return outroTypeName;
+    }
+    public void setOutroTypeName(String outroTypeName) {
+        this.outroTypeName = outroTypeName;
+    }
+
+    /*Pontos*/
+    public int getPoints(){
+        return points;
+    }
+    public void addPoints(int points) {
+        this.points = this.points + points;
+    }
+
+    /*Movimento*/
     public void move(){
         switch (this.orientation){
             case ("Norte"): {
@@ -94,74 +103,53 @@ public abstract class Creature extends Element {
             }
         }
     }
-
     public void spin(){
-            switch (this.orientation){
-                case ("Norte"):
-                    this.orientation = "Este";
-                    this.image = this.outroTypeName+"-"+this.orientation+".png";
-                    break;
-                case ("Este"):
-                    this.orientation = "Sul";
-                    this.image = this.outroTypeName+"-"+this.orientation+".png";
-                    break;
-                case ("Sul"):
-                    this.orientation = "Oeste";
-                    this.image = this.outroTypeName+"-"+this.orientation+".png";
-                    break;
-                case ("Oeste"):
-                    this.orientation = "Norte";
-                    this.image = this.outroTypeName+"-"+this.orientation+".png";
-                    break;
-            }
+        switch (this.orientation){
+            case ("Norte"):
+                this.orientation = "Este";
+                this.image = this.outroTypeName+"-"+this.orientation+".png";
+                break;
+            case ("Este"):
+                this.orientation = "Sul";
+                this.image = this.outroTypeName+"-"+this.orientation+".png";
+                break;
+            case ("Sul"):
+                this.orientation = "Oeste";
+                this.image = this.outroTypeName+"-"+this.orientation+".png";
+                break;
+            case ("Oeste"):
+                this.orientation = "Norte";
+                this.image = this.outroTypeName+"-"+this.orientation+".png";
+                break;
+        }
     }
 
-    public int getTeamId() {
-        return teamId;
+    /*Posição*/
+    public int getRange() {
+        return range;
     }
-
-    public int getCost(){ return cost; }
-
+    public int getNextX() {
+        return nextX;
+    }
+    public void setNextX(int nextX) {
+        this.nextX = nextX;
+    }
+    public int getNextY() {
+        return nextY;
+    }
+    public void setNextY(int nextY) {
+        this.nextY = nextY;
+    }
     public String getOrientation(){
         return orientation;
     }
-
     public void setOrientation(String orientation){
         this.orientation = orientation;
     }
 
-    public int getPoints(){
-        return points;
-    }
-
-    public void addPoints(int points) {
-        this.points = this.points + points;
-    }
-
-    //public void lookAtTheWorld(int rows, int columns, List<Hole> holes, List<Treasure> treasures, List<Creature> creatures) {} -----> NOT YET!
-
-    public int getRange() {
-        return range;
-    }
-
-    public boolean isEnchant() {
-        return isEnchant;
-    }
-
-    public void setEnchant(boolean enchant) {
-        isEnchant = enchant;
-    }
-
-    public String getItSpellName() {
-        return itSpellName;
-    }
-
-    public void setItSpellName(String itSpellName) {
-        this.itSpellName = itSpellName;
-    }
-
-    public boolean isFrozen() {
-        return isFrozen;
+    /* Feitiços */
+    public void congela4Ever() {
+        isFrozen4Ever= true;
     }
     public void descongela(){
         this.isFrozen4Ever =false; // Custa 8 Inverte aplicação do Freezes4Ever.
@@ -170,49 +158,6 @@ public abstract class Creature extends Element {
         this.isFrozen = true;
         this.frozenTime = 0;
     }
-
-    public int getFrozenTime() {
-        return frozenTime;
-    }
-    public void congela4Ever() {
-        isFrozen4Ever= true;
-    }
-
-    public void setFrozenTime(int frozenTime) {
-        this.frozenTime = frozenTime;
-    }
-
-    public void setFrozen(boolean frozen) {
-        isFrozen = frozen;
-    }
-
-    public boolean isFrozen4Ever() {
-        return isFrozen4Ever;
-    }
-
-    /*public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }*/
-
-    public int getNextX() {
-        return nextX;
-    }
-
-    public void setNextX(int nextX) {
-        this.nextX = nextX;
-    }
-
-    public int getNextY() {
-        return nextY;
-    }
-
-    public void setNextY(int nextY) {
-        this.nextY = nextY;
-    }
-
-
-    /* Feitiços */
-
     public void empurraParaNorte() {
         this.y = this.y -1;
         // Custa 1 Move 1 para Norte
@@ -236,7 +181,35 @@ public abstract class Creature extends Element {
         this.range = range *2;
          // Custa 3 Aumenta alcance para o dobro
     }
+    public boolean isEnchant() {
+        return isEnchant;
+    }
+    public void setEnchant(boolean enchant) {
+        isEnchant = enchant;
+    }
+    public String getItSpellName() {
+        return itSpellName;
+    }
+    public void setItSpellName(String itSpellName) {
+        this.itSpellName = itSpellName;
+    }
+    public boolean isFrozen() {
+        return isFrozen;
+    }
+    public int getFrozenTime() {
+        return frozenTime;
+    }
+    public void setFrozenTime(int frozenTime) {
+        this.frozenTime = frozenTime;
+    }
+    public void setFrozen(boolean frozen) {
+        isFrozen = frozen;
+    }
+    public boolean isFrozen4Ever() {
+        return isFrozen4Ever;
+    }
 
+    /*Tesouros*/
     public void addGold() {
         gold = gold +1;
     }
@@ -255,12 +228,13 @@ public abstract class Creature extends Element {
     public int getBronze() {
         return bronze;
     }
-
     public int getCollectedTreasures() {
         return collectedTreasures;
     }
-
     public void collectTreasure(int value) {
         this.collectedTreasures ++;
     }
+
+    //public void lookAtTheWorld(int rows, int columns, List<Hole> holes, List<Treasure> treasures, List<Creature> creatures) {} -----> NOT YET!
+
 }
